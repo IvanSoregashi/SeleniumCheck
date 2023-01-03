60400116b476db58f2013933c182177de2821124
Ivan S <IvanShiriaev2222@gmail.com>
2023-01-03 17:23:45 +0600

    classed the requests

--- api_test.py
+++ api_test.py
@@ -1,11 +1,61 @@
 from requests import request
+import json
+from users import User
 
-email = 'email@email.com'
-url = f"http://users.bugred.ru/tasks/rest/getuser?email={email}"
 
-payload = ""
-headers = {}
+class Req:
 
-response = request("POST", url)
+    def get_user(self, email='email@email.com'):
+        url = f"http://users.bugred.ru/tasks/rest/getuser?email={email}"
+        return request("POST", url)
 
-print(response.text)
+    def do_register(self, user=User()):
+        url = "http://users.bugred.ru/tasks/rest/doregister"
+        payload = json.dumps(user.to_dict())
+        headers = {'Content-Type': 'application/json'}
+        return request("POST", url, headers=headers, data=payload)
+
+    def create_user(self):
+        url = "http://users.bugred.ru/tasks/rest/createuser"
+        payload = json.dumps({"email": "dododo@mail.ru", "name": "Stefan", "companies": [10], "tasks": [12]})
+        headers = {'Content-Type': 'application/json'}
+        response = request("POST", url, headers=headers, data=payload)
+        return response.text
+
+    def create_company(self):
+        url = "http://users.bugred.ru/tasks/rest/createcompany"
+        payload = {'company_name': 'zaraaa', 'company_type': 'ООО',
+                   'company_users': '["elena345@mail.ru", "day234@mail.ru"]', 'email_owner': 'alyona111@mail.ru'}
+        files = []
+        headers = {}
+        response = request("POST", url, headers=headers, data=payload, files=files)
+        return response.text
+
+    def create_user_with_tasks(self):
+        url = "http://users.bugred.ru/tasks/rest/createuserwithtasks"
+        payload = json.dumps({
+            "email": "ghilbertt@mail.ru",
+            "name": "Elina",
+            "tasks": [
+                {
+                    "title": "Первая задача",
+                    "description": "Первая задача 11"
+                },
+                {
+                    "title": "Вторая задача",
+                    "description": "Вторая задача 11"
+                }
+            ]
+        })
+        headers = {'Content-Type': 'application/json'}
+        response = request("POST", url, headers=headers, data=payload)
+        return response.text
+
+
+r = Req()
+usr = User()
+usr.from_file('users.json')
+resp = r.do_register(usr)
+print(resp.text)
+print(resp.content)
+print(resp.content.decode('unicode-escape'))
